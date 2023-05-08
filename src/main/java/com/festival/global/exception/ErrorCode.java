1c41c5c2005555c2cc1320e30915dd3267259938
SoohoLee <hyeri0603@naver.com>
2023-05-09 00:00:12 +0900

    feat: Pub 에러코드, 예외처리 추가 (#20)

--- src/main/java/com/festival/global/exception/ErrorCode.java
+++ src/main/java/com/festival/global/exception/ErrorCode.java
@@ -5,11 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    USER_NOT_FOUND(404, "ACCOUNT-001", "회원이 존재하지 않는 경우"),
-    USER_NOT_MATCH(403, "ACCOUNT-002", "회원 권한이 다를 경우"),
-    MAXSIZE_OVER(400, "ACCOUNT-003", String.format("파일은 최대 %sMB 크기까지 입력할 수 있습니다.", 3)),
-    PUB_NOT_FOUND(404, "DIARY-001", "주점이 존재하지 않는 경우"),
-    PUB_CONTENT_NOT_FOUND(404, "DIARY-002", "주점의 이미지가 존재하지 않는 경우");
+    ADMIN_NOT_FOUND(404, "ADMIN-001", "관리자가 존재하지 않는 경우"),
+    ADMIN_NOT_MATCH(403, "ADMIN-002", "관리자 권한이 다를 경우"),
+    MAXSIZE_OVER(400, "ADMIN-003", String.format("파일은 최대 %sMB 크기까지 입력할 수 있습니다.", 3)),
+    PUB_NOT_FOUND(404, "PUB-001", "주점이 존재하지 않는 경우"),
+    PUB_CONTENT_NOT_FOUND(404, "PUB-002", "주점의 이미지가 존재하지 않는 경우");
 
     private final int status;
     private final String code;
--- src/main/java/com/festival/global/exception/GlobalExceptionAdvice.java
+++ src/main/java/com/festival/global/exception/GlobalExceptionAdvice.java
@@ -1,6 +1,8 @@
 package com.festival.global.exception;
 
 import com.festival.domain.admin.exception.AdminException;
+import com.festival.domain.admin.exception.AdminNotMatchException;
+import com.festival.domain.info.festivalPub.exception.PubNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,7 +13,23 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionAdvice {
 
     @ExceptionHandler(AdminException.class)
-    public ResponseEntity<ErrorResponse> AdminHandleException(AdminException e) {
+    public ResponseEntity<ErrorResponse> AdminNotFoundHandleException(AdminException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.error("[exceptionHandle] ex", e);
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(AdminNotMatchException.class)
+    public ResponseEntity<ErrorResponse> AdminNotMatchHandleException(AdminNotMatchException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.error("[exceptionHandle] ex", e);
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), e.getMessage());
+        return ResponseEntity.status(errorCode.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(PubNotFoundException.class)
+    public ResponseEntity<ErrorResponse> PubNotFoundHandleException(PubNotFoundException e) {
         ErrorCode errorCode = e.getErrorCode();
         log.error("[exceptionHandle] ex", e);
         ErrorResponse errorResponse = new ErrorResponse(errorCode.getCode(), e.getMessage());
