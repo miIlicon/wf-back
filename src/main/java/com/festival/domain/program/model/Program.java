438ba6d8315986f35aa19cea44f4ecf3d3498b03
Abbbyy <kimkim200011@gmail.com>
2023-08-27 19:30:10 +0900

    feat: program Entity 생성

--- src/main/java/com/festival/domain/program/model/Program.java
+++ src/main/java/com/festival/domain/program/model/Program.java
@@ -1,4 +1,41 @@
 package com.festival.domain.program.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Program {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "sub_title", nullable = false)
+    private String subTitle;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "latitude", nullable = false) // 위도
+    private float latitude;
+
+    @Column(name = "longitude", nullable = false) // 경도
+    private float longitude;
+
+    @Column(name = "status", nullable = false)
+    private ItemStatus status;
+
+    @Column(name = "type", nullable = false)
+    private ItemType type;
+
+    public enum ItemStatus {
+        OPERATE, TERMINATE
+    }
+
+    public enum ItemType {
+        PROGRAM, GAME
+    }
 }
