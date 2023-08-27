af384f89b562093e29a70b89736524a3b2760bf1
SoohoLee <hyeri0603@naver.com>
2023-08-27 21:57:55 +0900

    feat: BamBooForest 컨트롤러 구현 (#98)

--- src/main/java/com/festival/domain/bambooforest/controller/BambooForestController.java
+++ src/main/java/com/festival/domain/bambooforest/controller/BambooForestController.java
@@ -1,4 +1,34 @@
 package com.festival.domain.bambooforest.controller;
 
+import com.festival.domain.bambooforest.dto.BamBooForestCreateReq;
+import com.festival.domain.bambooforest.dto.BamBooForestRes;
+import com.festival.domain.bambooforest.service.BamBooForestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v2/bambooforest")
 public class BambooForestController {
+
+    private final BamBooForestService bambooForestService;
+
+    @PostMapping
+    public ResponseEntity<Long> createBamBooForest(@RequestBody BamBooForestCreateReq bamBooForestCreateReq) {
+        return ResponseEntity.ok().body(bambooForestService.create(bamBooForestCreateReq));
+    }
+
+    @DeleteMapping("/{bamBooForestId}")
+    public ResponseEntity<Void> deleteBamBooForest(@PathVariable Long bamBooForestId) {
+        bambooForestService.delete(bamBooForestId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<Page<BamBooForestRes>> getList(String status, Pageable pageable) {
+        return ResponseEntity.ok().body(bambooForestService.getList(status, pageable));
+    }
 }
