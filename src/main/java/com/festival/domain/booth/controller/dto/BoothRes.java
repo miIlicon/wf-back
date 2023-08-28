f0c424f7d1bfc10395f6d752f97e2ae78ca0af46
SoohoLee <hyeri0603@naver.com>
2023-08-28 16:27:00 +0900

    feat: Guide 컨트롤러 구현 (#105)

--- src/main/java/com/festival/domain/booth/controller/dto/BoothRes.java
+++ src/main/java/com/festival/domain/booth/controller/dto/BoothRes.java
@@ -28,7 +28,8 @@ public class BoothRes {
 
     @Builder
     @QueryProjection
-    public BoothRes(String title, String subTitle, String content, float latitude, float longitude, String status, String type) {
+    public BoothRes(Long id, String title, String subTitle, String content, float latitude, float longitude, String status, String type) {
+        this.id = id;
         this.title = title;
         this.subTitle = subTitle;
         this.content = content;
--- src/main/java/com/festival/domain/booth/repository/BoothRepository.java
+++ src/main/java/com/festival/domain/booth/repository/BoothRepository.java
@@ -1,7 +1,8 @@
 package com.festival.domain.booth.repository;
 
 import com.festival.domain.booth.model.Booth;
+import com.festival.domain.guide.repository.GuideRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BoothRepository extends JpaRepository<Booth, Long> {
+public interface BoothRepository extends JpaRepository<Booth, Long>, BoothRepositoryCustom {
 }
