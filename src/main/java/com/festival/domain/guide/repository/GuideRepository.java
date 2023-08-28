7b090dd160f78e90d496d7b60dc7e8de9290051b
SoohoLee <hyeri0603@naver.com>
2023-08-28 16:19:55 +0900

    feat: Guide 리포지토리 구현 (#105)

--- src/main/java/com/festival/domain/guide/repository/GuideRepository.java
+++ src/main/java/com/festival/domain/guide/repository/GuideRepository.java
@@ -1,4 +1,7 @@
 package com.festival.domain.guide.repository;
 
-public interface GuideRepository {
+import com.festival.domain.guide.model.Guide;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuideRepository extends JpaRepository<Guide, Long>, GuideRepositoryCustom {
 }
---
+++ src/main/java/com/festival/domain/guide/repository/vo/GuideSearchCond.java
@@ -0,0 +1,8 @@
+package com.festival.domain.guide.repository.vo;
+
+import lombok.Getter;
+
+@Getter
+public class GuideSearchCond {
+    private String status;
+}
