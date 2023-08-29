51ce4af4f9be4c94349d0669f21fe6ab217fecc1
SoohoLee <hyeri0603@naver.com>
2023-08-29 22:13:30 +0900

    feat: Member 컨트롤러 구현(#107)

--- src/main/java/com/festival/domain/member/controller/MemberController.java
+++ src/main/java/com/festival/domain/member/controller/MemberController.java
@@ -1,4 +1,22 @@
 package com.festival.domain.member.controller;
 
+import com.festival.domain.member.dto.MemberReq;
+import com.festival.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/member")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ResponseEntity<Void> join(@RequestBody @Valid MemberReq memberReq) {
+        memberService.join(memberReq);
+        return ResponseEntity.ok().build();
+    }
 }
