7df55ef149d9e5cb796b38baf06338bbdb807486
SoohoLee <hyeri0603@naver.com>
2023-08-30 20:02:26 +0900

    refactor: Jwt 인증 방식 변경으로 인한 로직 수정 (#119)

---
+++ src/main/java/com/festival/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.festival.domain.member.repository;
+
+import com.festival.domain.member.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByLoginId(String loginId);
+    Optional<Member> findByLoginId(String loginId);
+}
