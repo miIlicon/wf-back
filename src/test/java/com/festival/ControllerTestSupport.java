be83fca1cd55d50814f8319d60343afa0dc6dfb2
SoohoLee <hyeri0603@naver.com>
2023-08-31 22:23:20 +0900

    test: 컨트롤러 통합테스트 지원 클래스 구성 (#124)

---
+++ src/test/java/com/festival/ControllerTestSupport.java
@@ -0,0 +1,24 @@
+package com.festival;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.festival.common.security.SecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles("test")
+@Import(SecurityConfig.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+}
