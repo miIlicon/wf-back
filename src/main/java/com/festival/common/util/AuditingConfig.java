2e452c3f3a19473f1ee61ce33847d0f7ca5a2e96
SoohoLee <hyeri0603@naver.com>
2023-08-31 22:20:28 +0900

    refactor: enable Auditing (#124)

--- src/main/java/com/festival/common/util/AuditingConfig.java
+++ src/main/java/com/festival/common/util/AuditingConfig.java
@@ -1,7 +1,9 @@
 package com.festival.common.util;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
 }
