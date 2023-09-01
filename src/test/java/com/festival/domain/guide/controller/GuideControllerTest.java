36e2f55eb1271fc279b3f05169bb5fecc47ee5c5
SoohoLee <hyeri0603@naver.com>
2023-09-01 20:20:52 +0900

    feat: Guide 통합 테스트 로직 추가 (미완성) (#131)

---
+++ src/test/java/com/festival/domain/guide/controller/GuideControllerTest.java
@@ -0,0 +1,214 @@
+package com.festival.domain.guide.controller;
+
+import com.festival.domain.guide.dto.GuideRes;
+import com.festival.domain.guide.model.Guide;
+import com.festival.domain.guide.repository.GuideRepository;
+import com.festival.domain.image.fixture.ImageFixture;
+import com.festival.domain.member.model.Member;
+import com.festival.domain.member.repository.MemberRepository;
+import com.festival.domain.util.ControllerTestSupport;
+import com.festival.domain.guide.dto.GuideReq;
+import com.festival.domain.util.TestImageUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static com.festival.domain.guide.model.GuideStatus.*;
+import static com.festival.domain.guide.model.GuideType.*;
+import static com.festival.domain.member.model.MemberRole.ADMIN;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.MediaType.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+class GuideControllerTest extends ControllerTestSupport {
+
+    @Autowired
+    private GuideRepository guideRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    private Member member;
+
+    @BeforeEach
+    void setUp() {
+        member = Member.builder()
+                .username("testUser")
+                .password("12345")
+                .memberRole(ADMIN)
+                .build();
+        memberRepository.saveAndFlush(member);
+    }
+
+    @WithMockUser(username = "testUser", roles = "ADMIN")
+    @DisplayName("안내사항 게시물을 등록한다.")
+    @Test
+    void createGuide() throws Exception {
+        //given
+        MockMultipartFile mainFile = TestImageUtils.generateMockImageFile("mainFile");
+        List<MockMultipartFile> subFiles = List.of(
+                TestImageUtils.generateMockImageFile("subFiles"),
+                TestImageUtils.generateMockImageFile("subFiles"),
+                TestImageUtils.generateMockImageFile("subFiles")
+        );
+
+        GuideReq guideReq = GuideReq.builder()
+                .title("title")
+                .content("content")
+                .type("QNA")
+                .status("OPERATE")
+                .build();
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(
+                        multipart("/api/v2/guide")
+                                .file(mainFile)
+                                .file(subFiles.get(0))
+                                .file(subFiles.get(1))
+                                .file(subFiles.get(2))
+                                .param("title", guideReq.getTitle())
+                                .param("content", guideReq.getContent())
+                                .param("type", guideReq.getType())
+                                .param("status", guideReq.getStatus())
+                                .contentType(MULTIPART_FORM_DATA)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        //then
+        String content = mvcResult.getResponse().getContentAsString();
+        Long id = objectMapper.readValue(content, Long.class);
+        Guide findGuide = guideRepository.findById(id).get();
+        assertThat(findGuide).isNotNull()
+                .extracting("title", "content", "guideType", "guideStatus")
+                .contains("title", "content", QNA, OPERATE);
+    }
+
+    @WithMockUser(username = "testUser", roles = "ADMIN")
+    @DisplayName("안내사항 게시물들의 내용 등을 수정한다.")
+    @Test
+    void updateGuide() throws Exception {
+        //given
+        GuideReq guideReq = GuideReq.builder()
+                .title("title")
+                .content("content")
+                .type("QNA")
+                .status("OPERATE")
+                .build();
+        Guide guide = Guide.of(guideReq);
+        guide.setImage(ImageFixture.image);
+        Guide savedGuide = guideRepository.saveAndFlush(guide);
+
+        //when
+        mockMvc.perform(
+                        put("/api/v2/guide/" + savedGuide.getId())
+                                .param("title", "updateTitle")
+                                .param("content", "updateContent")
+                                .param("type", "NOTICE")
+                                .param("status", "OPERATE")
+                                .contentType(MULTIPART_FORM_DATA)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("1"));
+
+        //then
+        Guide findGuide = guideRepository.findById(savedGuide.getId()).get();
+        assertThat(findGuide).isNotNull()
+                .extracting("title", "content", "guideType", "guideStatus")
+                .contains("updateTitle", "updateContent", NOTICE, OPERATE);
+    }
+
+    @WithMockUser(username = "testUser", roles = "ADMIN")
+    @DisplayName("안내사항 게시물중 하나를 선택하여 삭제한다.")
+    @Test
+    void deleteGuide() throws Exception {
+        //given
+        Guide guide = createGuideEntity("title", "content", "QNA", "OPERATE");
+        Guide savedGuide = guideRepository.saveAndFlush(guide);
+
+        //when
+        mockMvc.perform(
+                        delete("/api/v2/guide/" + savedGuide.getId())
+                                .contentType(APPLICATION_FORM_URLENCODED)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        //then
+        Guide findGuide = guideRepository.findById(savedGuide.getId()).get();
+        assertThat(findGuide.getGuideStatus()).isEqualTo(TERMINATE);
+    }
+
+    @DisplayName("안내사항 목록을 가져온다. 목록은 페이징으로 10개씩 처리된다. 테스트 환경에서는 5개의 데이터를 페이징하여 테스트한다.")
+    @Test
+    void getListGuide() throws Exception {
+        //given
+        String status = "OPERATE";
+        Pageable pageable = PageRequest.of(0, 5);
+
+        Guide guide1 = createGuideEntity("title1", "content1", "QNA", "OPERATE");
+        Guide guide2 = createGuideEntity("title2", "content2", "NOTICE", "OPERATE");
+        Guide guide3 = createGuideEntity("title3", "content3", "QNA", "OPERATE");
+        Guide guide4 = createGuideEntity("title4", "content4", "NOTICE", "OPERATE");
+        Guide guide5 = createGuideEntity("title5", "content5", "QNA", "OPERATE");
+        Guide guide6 = createGuideEntity("title6", "content6", "NOTICE", "OPERATE");
+        Guide guide7 = createGuideEntity("title7", "content7", "QNA", "OPERATE");
+        Guide guide8 = createGuideEntity("title8", "content8", "NOTICE", "OPERATE");
+
+        List<Guide> guides = guideRepository.saveAllAndFlush(List.of(guide1, guide2, guide3, guide4, guide5, guide6, guide7, guide8));
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(
+                        get("/api/v2/guide/list")
+                                .param("status", status)
+                                .param("page", String.valueOf(pageable.getPageNumber()))
+                                .param("size", String.valueOf(pageable.getPageSize()))
+                                .contentType(APPLICATION_FORM_URLENCODED)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.content", hasSize(5)))
+                .andReturn();
+
+        //then
+        String content = mvcResult.getResponse().getContentAsString();
+        List<GuideRes> guideResList = objectMapper.readValue(content, objectMapper.getTypeFactory().constructCollectionType(List.class, GuideRes.class));
+        assertThat(guideResList).hasSize(5)
+                .extracting("title", "content", "guideType", "guideStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("title1", "content1", QNA, OPERATE),
+                        tuple("title2", "content2", NOTICE, OPERATE),
+                        tuple("title3", "content3", QNA, OPERATE),
+                        tuple("title4", "content4", NOTICE, OPERATE),
+                        tuple("title5", "content5", QNA, OPERATE)
+                );
+    }
+
+    private Guide createGuideEntity(String title, String content, String type, String status) {
+        GuideReq guideReq = GuideReq.builder()
+                .title(title)
+                .content(content)
+                .type(type)
+                .status(status)
+                .build();
+        Guide guide = Guide.of(guideReq);
+        guide.setImage(ImageFixture.image);
+        guide.connectMember(member);
+        return guide;
+    }
+}
--- src/test/java/com/festival/domain/util/ControllerTestSupport.java
+++ src/test/java/com/festival/domain/util/ControllerTestSupport.java
@@ -1,11 +1,9 @@
-package com.festival;
+package com.festival.domain.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.festival.common.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
