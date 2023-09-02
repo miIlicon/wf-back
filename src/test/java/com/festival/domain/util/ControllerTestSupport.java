586d8ce12f281dbdbd1caa852f20b6098dc0902a
SoohoLee <hyeri0603@naver.com>
2023-09-02 21:36:13 +0900

    test: memberRepository 지원클래스 이동 (#145)

--- src/test/java/com/festival/domain/util/ControllerTestSupport.java
+++ src/test/java/com/festival/domain/util/ControllerTestSupport.java
@@ -1,6 +1,7 @@
 package com.festival.domain.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.festival.domain.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,4 +21,6 @@ public abstract class ControllerTestSupport {
     @Autowired
     protected ObjectMapper objectMapper;
 
+    @Autowired
+    protected MemberRepository memberRepository;
 }
