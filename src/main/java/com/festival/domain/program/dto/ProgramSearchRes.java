286d43acc07412d05952a2c830b36941b4f76fff
gkfktkrh153 <gkfktkrh153@naver.com>
2023-09-10 15:47:00 +0900

    Feature: Program 검색 기능 추가

---
+++ src/main/java/com/festival/domain/program/dto/ProgramSearchRes.java
@@ -0,0 +1,29 @@
+package com.festival.domain.program.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ProgramSearchRes {
+
+    private Long id;
+
+    private String title;
+
+    private String subTitle;
+
+    private String status;
+
+    private String mainImagePath;
+
+    @QueryProjection
+    public ProgramSearchRes(Long id, String title, String subTitle, String status, String mainImagePath) {
+        this.id = id;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.status = status;
+        this.mainImagePath = mainImagePath;
+    }
+}
