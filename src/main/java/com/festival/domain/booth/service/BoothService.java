faa9632ea8be4d1d647827df11013ea2d6a827db
SoohoLee <hyeri0603@naver.com>
2023-10-02 16:05:53 +0900

    refactor: S3로직으로 변경 (#323)

--- src/main/java/com/festival/domain/booth/service/BoothService.java
+++ src/main/java/com/festival/domain/booth/service/BoothService.java
@@ -37,7 +37,7 @@ public class BoothService {
 
     public Long createBooth(BoothReq boothReq) {
         Booth booth = Booth.of(boothReq);
-        booth.setImage(imageService.createImage(boothReq.getMainFile(), boothReq.getSubFiles(), boothReq.getType()));
+        booth.setImage(imageService.uploadImage(boothReq.getMainFile(), boothReq.getSubFiles(), boothReq.getType()));
         booth.connectMember(memberService.getAuthenticationMember());
         return boothRepository.save(booth).getId();
     }
@@ -49,12 +49,9 @@ public class BoothService {
             throw new ForbiddenException(FORBIDDEN_UPDATE);
         }
 
-        /**
-         * @Todo
-         *  delete로직 작성해야함
-         */
+        imageService.deleteImage(booth.getImage());
+        booth.setImage(imageService.uploadImage(boothReq.getMainFile(), boothReq.getSubFiles(), boothReq.getType()));
         booth.update(boothReq);
-        booth.setImage(imageService.createImage(boothReq.getMainFile(), boothReq.getSubFiles(), boothReq.getType()));
         return id;
     }
 
