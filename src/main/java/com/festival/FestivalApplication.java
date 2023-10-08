9cc4348171247d22ee3b53ab834bc7142486d256
seungnYong <77851079+gkfktkrh153@users.noreply.github.com>
2023-10-09 02:58:51 +0900

    Update FestivalApplication.java

--- src/main/java/com/festival/FestivalApplication.java
+++ src/main/java/com/festival/FestivalApplication.java
@@ -12,4 +12,13 @@ public class FestivalApplication {
     public static void main(String[] args) {
         SpringApplication.run(FestivalApplication.class, args);
     }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://www.withfestival.site");
+            }
+        };
+    }
 }
