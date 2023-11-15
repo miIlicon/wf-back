e11c4e835a85b33eec1489a4893d37b4e924c340
SoohoLee <hyeri0603@naver.com>
2023-11-15 21:13:50 +0900

    refactor: 미사용 메소드 제거 (#411)

--- src/main/java/com/festival/domain/booth/repository/BoothRepository.java
+++ src/main/java/com/festival/domain/booth/repository/BoothRepository.java
@@ -1,5 +1,6 @@
 package com.festival.domain.booth.repository;
 
+import com.festival.common.base.OperateStatus;
 import com.festival.domain.booth.model.Booth;
 import com.festival.domain.guide.repository.GuideRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,8 +9,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface BoothRepository extends JpaRepository<Booth, Long>, BoothRepositoryCustom {
-    Booth getBoothByTitle(String title);
-
-    List<Booth> findByStartDateEqualsAndOperateStatusEquals(LocalDate now, String value);
-    List<Booth> findByEndDateEquals(LocalDate now);
+    List<Booth> findByStartDateEqualsAndOperateStatusEquals(LocalDate inputDate, OperateStatus operateStatus);
+    List<Booth> findByEndDateEquals(LocalDate inputDate);
 }
