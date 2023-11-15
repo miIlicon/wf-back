8f66e52e0030d206be40f14aa333b5239f2dd57b
SoohoLee <hyeri0603@naver.com>
2023-11-15 21:16:40 +0900

    test: 안내사항 케이스 추가 (#411)

--- src/test/java/com/festival/domain/guide/model/GuideTest.java
+++ src/test/java/com/festival/domain/guide/model/GuideTest.java
@@ -19,4 +19,19 @@ class GuideTest {
         //then
         assertThat(guide.isDeleted()).isFalse();
     }
+
+    @DisplayName("안내사항 게시물의 조회수가 입력된 만큼 증가한다.")
+    @Test
+    void increaseViewCount() throws Exception {
+        //given
+        Guide guide = Guide.of(GuideReq.builder()
+                .content("test1")
+                .build());
+
+        //when
+        guide.increaseViewCount(3L);
+
+        //then
+        assertThat(guide.getViewCount()).isEqualTo(3L);
+    }
 }
